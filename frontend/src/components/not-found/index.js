215a3ab521f4584a90c3676bb3229d274c1a7571
Ross Nomann <rossnomann@protonmail.com>
2017-08-02 21:45:35 +0300

    Add "Not Found" page

---
+++ frontend/src/components/not-found/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css';
+
+const NotFound = () => (
+    <div className="not-found">
+        The requested page does not exist
+    </div>
+);
+
+export default NotFound;
