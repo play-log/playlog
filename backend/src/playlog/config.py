a98d07196bba84846bd2acdd3a03334aacae9ab3
Ross Nomann <rossnomann@protonmail.com>
2017-09-27 19:08:02 +0300

    Improve config.getenv function
    Don't allow empty environment variables.

--- backend/src/playlog/config.py
+++ backend/src/playlog/config.py
@@ -2,14 +2,16 @@ import os
 
 
 def getenv(key):
-    key = 'PLAYLOG_{}'.format(key)
-    value = os.getenv(key)
-    if value is None:
-        raise ValueError('Environment variable {} is not set'.format(key))
+    key = 'PLAYLOG_{}'.format(key.upper())
+    if key not in os.environ:
+        raise KeyError('Environment variable "{}" is not set'.format(key))
+    value = os.environ[key]
+    if not value:
+        raise ValueError('Environment variable "{}" can not be empty'.format(key))
     return value
 
 
-DEBUG = getenv('DEBUG')
+DEBUG = getenv('debug')
 if DEBUG not in ['false', 'true']:
     raise ValueError(
         'Unexpected "DEBUG" value: '
@@ -18,18 +20,18 @@ if DEBUG not in ['false', 'true']:
     )
 DEBUG = DEBUG == 'true'
 
-SERVER_HOST = getenv('SERVER_HOST')
-SERVER_PORT = int(getenv('SERVER_PORT'))
+SERVER_HOST = getenv('server_host')
+SERVER_PORT = int(getenv('server_port'))
 
-SA_URL = getenv('SA_URL')
-REDIS_URL = getenv('REDIS_URL').split(':')
+SA_URL = getenv('sa_url')
+REDIS_URL = getenv('redis_url').split(':')
 
-USER_NAME = getenv('USER_NAME')
-USER_EMAIL = getenv('USER_EMAIL')
+USER_NAME = getenv('user_name')
+USER_EMAIL = getenv('user_email')
 
 SUBMISSIONS = {
-    'base_url': getenv('SUBMISSIONS_BASE_URL'),
-    'handshake_timeout': int(getenv('SUBMISSIONS_HANDSHAKE_TIMEOUT')),
-    'username': getenv('SUBMISSIONS_USER'),
-    'password_hash': getenv('SUBMISSIONS_PASSWORD_HASH')
+    'base_url': getenv('submissions_base_url'),
+    'handshake_timeout': int(getenv('submissions_handshake_timeout')),
+    'username': getenv('submissions_user'),
+    'password_hash': getenv('submissions_password_hash')
 }
