4cc1855e6231005a9cc74ccc9a331cb9e121cd90
Ross Nomann <rossnomann@protonmail.com>
2017-10-02 00:14:30 +0300

    Add error middleware
    Log exceptions and format error response.

--- backend/src/playlog/middlewares.py
+++ backend/src/playlog/middlewares.py
@@ -1,7 +1,8 @@
 import logging
 
-from aiohttp.web import HTTPInternalServerError, Response, json_response
+from aiohttp.web import HTTPError, HTTPInternalServerError, Response, json_response
 from playlog.lib.json import dumps as json_encode
+from playlog.lib.validation import ValidationError
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +22,40 @@ async def submissions_middleware(app, next_handler):
     return handler
 
 
+class ErrorResponse(object):
+    def __init__(self, *, data, status):
+        self.data = data
+        self.status = status
+
+
+async def error_middleware(app, next_handler):
+    async def handler(request):
+        try:
+            result = await next_handler(request)
+        except Exception as exc:
+            if isinstance(exc, ValidationError):
+                result = ErrorResponse(data=exc.errors, status=400)
+            elif not isinstance(exc, HTTPError):
+                message = 'An error has occurred'
+                logger.exception(message)
+                result = ErrorResponse(data={'message': message}, status=500)
+        return result
+    return handler
+
+
 async def response_middleware(app, next_handler):
     async def handler(request):
         result = await next_handler(request)
         if not isinstance(result, Response):
             accept = request.headers.get('accept')
             if accept == 'application/json':
-                result = json_response(result, dumps=json_encode)
+                if isinstance(result, ErrorResponse):
+                    data = result.data
+                    status = result.status
+                else:
+                    data = result
+                    status = 200
+                result = json_response(data, status=status, dumps=json_encode)
             else:
                 logger.error('Unable to serialize response (accept=%s)', accept)
                 raise HTTPInternalServerError()
@@ -35,4 +63,4 @@ async def response_middleware(app, next_handler):
     return handler
 
 
-MIDDLEWARES = [submissions_middleware, response_middleware]
+MIDDLEWARES = [submissions_middleware, response_middleware, error_middleware]
