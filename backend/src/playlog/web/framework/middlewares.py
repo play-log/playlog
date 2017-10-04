291798d3e83de6c9aee362bea276087ba419601e
Ross Nomann <rossnomann@protonmail.com>
2017-10-04 20:16:29 +0300

    Format response for HTTP errors

--- backend/src/playlog/web/framework/middlewares.py
+++ backend/src/playlog/web/framework/middlewares.py
@@ -1,6 +1,6 @@
 import logging
 
-from aiohttp.web import HTTPError, HTTPInternalServerError, Response
+from aiohttp.web import HTTPException, HTTPInternalServerError, Response
 
 from playlog.lib.validation import ValidationError
 from playlog.web.framework.status import HTTP_BAD_REQUEST, HTTP_OK, HTTP_INTERNAL_SERVER_ERROR
@@ -20,7 +20,7 @@ async def error_middleware(app, next_handler):
                     data=exc.errors,
                     status=HTTP_BAD_REQUEST
                 )
-            elif not isinstance(exc, HTTPError):
+            elif not isinstance(exc, HTTPException):
                 message = 'An error has occurred'
                 logger.exception(message)
                 result = ErrorResponse(
@@ -28,7 +28,13 @@ async def error_middleware(app, next_handler):
                     status=HTTP_INTERNAL_SERVER_ERROR
                 )
             else:
-                raise exc  # TODO: format response
+                if exc.empty_body:
+                    raise exc
+                result = ErrorResponse(
+                    data={'message': exc.reason},
+                    status=exc.status,
+                    headers=exc.headers
+                )
         return result
     return handler
 
@@ -40,12 +46,12 @@ async def response_middleware(app, next_handler):
             accept = request.headers.get('accept')
             if accept == 'application/json':
                 if isinstance(result, ErrorResponse):
-                    data = result.data
-                    status = result.status
+                    data, status, headers = result.data, result.status, result.headers
+                    # since this is json response we don't need this header
+                    headers.pop('content-type', None)
                 else:
-                    data = result
-                    status = HTTP_OK
-                result = json_response(data, status=status)
+                    data, status, headers = result, HTTP_OK, None
+                result = json_response(data, status=status, headers=headers)
             else:
                 logger.error('Unable to serialize response (accept=%s)', accept)
                 raise HTTPInternalServerError()
--- backend/src/playlog/web/framework/response.py
+++ backend/src/playlog/web/framework/response.py
@@ -9,9 +9,10 @@ __all__ = ['ErrorResponse', 'json_response']
 
 
 class ErrorResponse(object):
-    def __init__(self, *, data, status):
+    def __init__(self, *, data, status, headers=None):
         self.data = data
         self.status = status
+        self.headers = headers
 
 
 json_response = functools.partial(_json_response, dumps=json_encode)
